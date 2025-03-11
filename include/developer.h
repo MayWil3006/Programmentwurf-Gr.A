d812e7c900bb829d9e9f0707bba7f18979ed61a0
MayWil3006 <willmann.maya-it24@it.dhbw-ravensburg.de>
2025-03-11 20:02:52 +0100

    Optimizations in developer.c and developer.h

--- include/developer.h
+++ include/developer.h
@@ -1,12 +1,16 @@
 #ifndef DEVELOPER_H_NHMPTW
 #define DEVELOPER_H_NHMPTW
 
+#define MAX_NAME 30
+#define MAX_ALIAS 30
+
 typedef struct {
-    char name[30];
-    char alias[30];
+    char name[MAX_NAME];
+    char alias[MAX_ALIAS];
 } Developer;
 
-Developer create_new_developer(char *name, char *alias); //function prototype to create a new object of type developer
-void print_new_developer(Developer *developer); //funtion prototype to print the developer's name and alias
+void create_new_developer(Developer *const developer, char name[], char alias[]); //function prototype to create a new object of type developer
+void print_new_developer(Developer const *const developer); //funtion prototype to print the developers details
 
 #endif DEVELOPER_H_NHMPTW 
+
--- src/developer.c
+++ src/developer.c
@@ -5,17 +5,35 @@
 #include "developer.h"
 #include <_string.h>
 
-Developer create_new_developer(char *name, char *alias) { //function definition: creates new developer
-    Developer developer;
+void create_new_developer(Developer *const developer, char name[], char alias[]) { //function definition: creates new developer
+    if(developer != NULL) {
+        strncpy(developer->name, name, sizeof(developer->name) - 1);
+        developer->name[sizeof(developer->name) - 1] = '\0';
+
+        strncpy(developer->alias, alias, sizeof(developer->alias) - 1);
+        developer->alias[sizeof(developer->alias) - 1] = '\0';
+    }
+    else {
+        printf("Pointer developer is NULL\n");
+    }
+    
+}
+    /*Developer developer;
     strncpy(developer.name, name, sizeof(developer.name) - 1);
     developer.name[sizeof(developer.name) - 1] = '\0';
 
     strncpy(developer.alias, alias, sizeof(developer.alias) - 1);
-    developer.alias[sizeof(developer.alias) - 1] = '\0';
+    developer.alias[sizeof(developer.alias) - 1] = '\0';*/
 
-    return developer;
-}
-void print_new_developer(Developer *developer) { //function definition: prints developer's name and alias
-    printf("Name: %s\n", developer->name);
-    printf("Alias: %s\n", developer->alias);
+    //return developer;
+
+void print_new_developer(Developer const *const developer) { //function definition: prints developer's name and alias
+    if(developer != NULL) {
+        printf("Developer details:\n");
+        printf("Name: %s\n", developer->name);
+        printf("Alias: %s\n", developer->alias);
+    }
+    else {
+        printf("Pointer developer for printing is NULL\n");
+    }
 }
