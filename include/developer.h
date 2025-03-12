57ebcee9005b39fac0d9b00fe0e49e4c82f37581
MayWil3006 <willmann.maya-it24@it.dhbw-ravensburg.de>
2025-03-12 20:14:36 +0100

    Changed names for better understanding

--- include/developer.h
+++ include/developer.h
@@ -1,16 +1,28 @@
 #ifndef DEVELOPER_H_NHMPTW
 #define DEVELOPER_H_NHMPTW
 
+/*
+developer.h defines developer struct
+and function prototypes
+*/
+
 #define MAX_NAME 30
 #define MAX_ALIAS 30
 
-typedef struct {
+typedef struct { //struct mit 2 char arrays die Namen und alias speichern
     char name[MAX_NAME];
     char alias[MAX_ALIAS];
 } Developer;
 
-void create_new_developer(Developer *const developer, char name[], char alias[]); //function prototype to create a new object of type developer
-void print_new_developer(Developer const *const developer); //funtion prototype to print the developers details
+/*
+function prototype to initialize a developer struct with a name and an alias
+*/
+void create_new_developer(Developer *const developer_ptr, const char name[], const char alias[]); 
 
-#endif DEVELOPER_H_NHMPTW 
+/*
+function prototype to print the developers details
+*/
+void print_new_developer(Developer const *const developer_ptr);
+
+#endif //DEVELOPER_H_NHMPTW 
 
--- src/developer.c
+++ src/developer.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "developer.h"
-#include <_string.h>
 
-void create_new_developer(Developer *const developer, char name[], char alias[]) { //function definition: creates new developer
-    if(developer != NULL) {
-        strncpy(developer->name, name, sizeof(developer->name) - 1);
-        developer->name[sizeof(developer->name) - 1] = '\0';
 
-        strncpy(developer->alias, alias, sizeof(developer->alias) - 1);
-        developer->alias[sizeof(developer->alias) - 1] = '\0';
+/*
+function initializes the developer struct with the name and alias
+*/
+
+void create_new_developer(Developer *const developer_ptr, const char name[], const char alias[]) { //function definition: creates new developer
+    if(developer_ptr != NULL) {
+        strncpy(developer_ptr->name, name, strlen(name) + 1); //kopiert den String name in developer_ptr->name
+        developer_ptr->name[strlen(name)] = '\0'; //setzt letztes Zeichen auf 0 um string zu beenden
+
+        strncpy(developer_ptr->alias, alias, strlen(alias) + 1);
+        developer_ptr->alias[strlen(alias)] = '\0';
     }
     else {
-        printf("Pointer developer is NULL\n");
+        printf("Pointer developer_ptr is NULL\n");
     }
     
 }
@@ -27,13 +30,15 @@ void create_new_developer(Developer *const developer, char name[], char alias[])
 
     //return developer;
 
-void print_new_developer(Developer const *const developer) { //function definition: prints developer's name and alias
-    if(developer != NULL) {
-        printf("Developer details:\n");
-        printf("Name: %s\n", developer->name);
-        printf("Alias: %s\n", developer->alias);
+void print_new_developer(Developer const *const developer_ptr) { //function definition: prints developer's name and alias
+    if(developer_ptr != NULL) {
+        printf("====================================\n");
+        printf("Developer: \n");
+        printf("Name: %s\n", developer_ptr->name);
+        printf("Alias: %s\n", developer_ptr->alias);
+        printf("====================================\n");
     }
     else {
-        printf("Pointer developer for printing is NULL\n");
+        printf("Pointer developer_ptr is NULL\n");
     }
 }
